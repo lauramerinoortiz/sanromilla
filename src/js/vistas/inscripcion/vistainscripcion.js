dfba21865a817169c84e48b9aaab11a7b25a8670
Antonio Moruno <antmoruno4@gmail.com>
2023-05-15 19:57:13 +0200

    UPDATE - Funcionamiento de botón añadir inscripción y registro de todos los datos de las inscripciones rellenadas.

--- src/js/vistas/inscripcion/vistainscripcion.js
+++ src/js/vistas/inscripcion/vistainscripcion.js
@@ -2,6 +2,7 @@
 import {InscripcionModel} from '../../modelos/inscripcionModel.js'
 export class VistaInscripcion{
     inscripciones = [];
+    inscripcionCounter = 1;
     constructor(controlador){
         this.controlador=controlador
         window.setTimeout(this.iniciar.bind(this), 500)
@@ -13,10 +14,19 @@ export class VistaInscripcion{
         console.log(this.controlador)
         let datos=await this.controlador.sacarCategorias()
         console.log(datos)
+        this.aceptarInscripciones = document.getElementById('aceptarInscripciones')
+        this.aceptarInscripciones.onclick = this.guardarDatosInscripciones.bind(this)
         this.fecha=document.getElementById('fecha')
         fecha.onchange=this.cargarCategoria.bind(this,datos)
         this.btnAnadirInscripcion = document.getElementById('btnAnadirInscripcion')
         this.btnAnadirInscripcion.onclick = this.anadirInscripcion.bind(this)
+
+        let h1 = document.createElement('h1');
+        h1.classList.add('d-flex', 'flex-column', 'text-center');
+        h1.textContent = `INSCRIPCIÓN ${this.inscripcionCounter}`;
+
+        let divInscripcion = document.getElementById('divInscripcion-inscripcion-1');
+        divInscripcion.children[0].appendChild(h1);
     }
 
     cargarCategoria(datos){
@@ -69,6 +79,31 @@ export class VistaInscripcion{
     }
 
     anadirInscripcion(){
+        this.inscripcionCounter ++;
+        let id = `inscripcion-${this.inscripcionCounter}`;
+
+        for(let i=1; i<this.inscripcionCounter; i++){
+            if (document.getElementById(`collapseInscripcion-inscripcion-${i}`).classList.contains('show')) {
+                document.getElementById(`collapseInscripcion-inscripcion-${i}`).classList.remove('show');
+            }
+        }
+
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('card','card-responsive','mt-5');
+        newDiv.id = `divInscripcion-inscripcion-${this.inscripcionCounter}`;
+
+        let divInscripcion = document.getElementById(`divInscripcion-inscripcion-${this.inscripcionCounter-1}`);
+        newDiv.innerHTML = divInscripcion.innerHTML;
+        divInscripcion.after(newDiv);
+        newDiv.children[0].querySelector('i').setAttribute('href',`#collapseInscripcion-${id}`);
+        newDiv.children[1].id =`collapseInscripcion-${id}`;
+
+        newDiv.querySelector('h1').textContent = `INSCRIPCIÓN ${this.inscripcionCounter}`;
+
+        newDiv.querySelector('#collapseInscripcion-' + id).classList.add('show');
+    }
+
+    /*guardarDatosInscripciones(){
         console.log('Añadiendo inscripción...');
         console.log(document.getElementsByTagName('input')[1].value);
 
@@ -89,16 +124,59 @@ export class VistaInscripcion{
             document.getElementsByTagName('input')[0].value,//nombre
             document.getElementsByTagName('input')[1].value,//apellidos
             generoSeleccionado,//genero
-            document.getElementsByTagName('input')[2].value,//fecha
-            document.getElementsByTagName('input')[3].value,//categoria
-            document.getElementsByTagName('input')[4].value,//precio
+            document.getElementsByTagName('input')[4].value,//fecha
+            document.getElementsByTagName('input')[5].value,//categoria
+            document.getElementsByTagName('input')[6].value,//precio
             tallaCamisetaSeleccionada,//camiseta
-            document.getElementsByTagName('input')[6].value,//dni
-            document.getElementsByTagName('input')[7].value,//email
-            document.getElementsByTagName('input')[8].value,//telefono
-            document.getElementsByTagName('input')[9].value,//info
+            document.getElementsByTagName('input')[7].value,//dni
+            document.getElementsByTagName('input')[8].value,//email
+            document.getElementsByTagName('input')[9].value,//telefono
+            document.getElementsByTagName('textarea')[0].value,//info
         ))
 
         console.log(this.inscripciones)
+    }*/
+
+    guardarDatosInscripciones() {
+        console.log('Guardando datos de inscripciones...');
+
+        this.inscripciones = [];
+
+        // Seleccionar todos los div de inscripción
+        const divsInscripcion = document.querySelectorAll('.card-responsive');
+
+        // Iterar sobre cada div de inscripción y extraer la información de los campos de entrada
+        for (let i = 0; i < divsInscripcion.length; i++) {
+            const inputs = divsInscripcion[i].querySelectorAll('input');
+            const textarea = divsInscripcion[i].querySelector('textarea');
+            const generoRadios = divsInscripcion[i].querySelectorAll('input[type="radio"][name="genero"]');
+            const camisetaSelect = divsInscripcion[i].querySelector('#camiseta');
+
+            let generoSeleccionado = '';
+            for (let j = 0; j < generoRadios.length; j++) {
+                if (generoRadios[j].checked) {
+                    generoSeleccionado = generoRadios[j].value;
+                    break;
+                }
+            }
+
+            const nuevaInscripcion = new InscripcionModel(
+                inputs[0].value, // nombre
+                inputs[1].value, // apellidos
+                generoSeleccionado, // genero
+                inputs[4].value, // fecha
+                inputs[5].value, // categoria
+                inputs[6].value, // precio
+                camisetaSelect.options[camisetaSelect.selectedIndex].value, // camiseta
+                inputs[7].value, // dni
+                inputs[8].value, // email
+                inputs[9].value, // telefono
+                textarea.value // info
+            );
+
+            this.inscripciones.push(nuevaInscripcion);
+        }
+
+        console.log(this.inscripciones);
     }
 }
