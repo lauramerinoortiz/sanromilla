23152a85b511e15db03e58165b0202b18f2979b5
Antonio Moruno <antmoruno4@gmail.com>
2023-05-20 14:34:24 +0200

    UPDATE - Carga de inscripciones cuando se vuelve desde la vista confirmación

--- src/js/vistas/inscripcion/vistainscripcion.js
+++ src/js/vistas/inscripcion/vistainscripcion.js
@@ -7,6 +7,7 @@ export class VistaInscripcion{
 
         /* Lista de inscripciones que vienen de vuelta */
         this.inscripcionesVolver = datos;
+        console.log('datos que vienen de vuelta: ', this.inscripcionesVolver);
 
         window.setTimeout(this.iniciar.bind(this), 500)
     }
@@ -28,12 +29,7 @@ export class VistaInscripcion{
         this.btnAnadirInscripcion = document.getElementById('btnAnadirInscripcion')
         this.btnAnadirInscripcion.onclick = this.anadirInscripcion.bind(this)
 
-        let h1 = document.createElement('h1');
-        h1.classList.add('d-flex', 'flex-column', 'text-center');
-        h1.textContent = `INSCRIPCIÓN ${this.inscripcionCounter}`;
-
-        let divInscripcion = document.getElementById('divInscripcion-inscripcion-1');
-        divInscripcion.children[0].appendChild(h1);
+        await this.cargarInscripciones();
     }
 
     /**
@@ -94,10 +90,8 @@ export class VistaInscripcion{
      * Añade una inscripción más al componente
      */
     anadirInscripcion(){
-        this.inscripcionCounter ++;
-        let id = `inscripcion-${this.inscripcionCounter}`;
-
-        for(let i=1; i<this.inscripcionCounter; i++){
+        //Cierra todos los divs que estén abiertos.
+        for(let i=1; i<=this.inscripcionCounter; i++){
             if (document.getElementById(`collapseInscripcion-inscripcion-${i}`).classList.contains('show')) {
                 document.getElementById(`collapseInscripcion-inscripcion-${i}`).classList.remove('show');
             }
@@ -105,28 +99,23 @@ export class VistaInscripcion{
 
         let newDiv = document.createElement('div');
         newDiv.classList.add('card','card-responsive','mt-5');
-        newDiv.id = `divInscripcion-inscripcion-${this.inscripcionCounter}`;
-
-        let divInscripcion = document.getElementById(`divInscripcion-inscripcion-${this.inscripcionCounter-1}`);
+        newDiv.id = `divInscripcion-inscripcion-${this.inscripcionCounter + 1}`;
+        let divInscripcion = document.getElementById(`divInscripcion-inscripcion-${this.inscripcionCounter}`);
         newDiv.innerHTML = divInscripcion.innerHTML;
         divInscripcion.after(newDiv);
-        newDiv.children[0].querySelector('i').setAttribute('href',`#collapseInscripcion-${id}`);
-        newDiv.children[1].id =`collapseInscripcion-${id}`;
-
-        newDiv.querySelector('h1').textContent = `INSCRIPCIÓN ${this.inscripcionCounter}`;
-
-        newDiv.querySelector('#collapseInscripcion-' + id).classList.add('show');
+        newDiv.children[0].querySelector('i').setAttribute('href',`#collapseInscripcion-inscripcion-${this.inscripcionCounter + 1}`);
+        newDiv.children[1].id =`collapseInscripcion-inscripcion-${this.inscripcionCounter + 1}`;
+        newDiv.querySelector('h1').textContent = `INSCRIPCIÓN ${this.inscripcionCounter + 1}`;
+        newDiv.querySelector(`#collapseInscripcion-inscripcion-${this.inscripcionCounter + 1}`).classList.add('show');
         this.fecha1=newDiv.querySelector('input[type="date"]');
         this.fecha1.onchange = this.cargarCategoria.bind(this, this.datos);
+        this.inscripcionCounter++;
     }
 
     /**
      * Guarda los datos de las incripciones en un array de objetos y los manda a la la vista de mostrar confirmación.
      */
     guardarDatosInscripciones() {
-        console.log('Guardando datos de inscripciones...');
-        this.inscripciones = [];
-
         // Seleccionar todos los div de inscripción
         const divsInscripcion = document.querySelectorAll('.card-responsive');
 
@@ -136,8 +125,8 @@ export class VistaInscripcion{
             const textarea = divsInscripcion[i].querySelector('textarea');
             const generoRadios = divsInscripcion[i].querySelectorAll('input[type="radio"][name="genero"]');
             const camisetaSelect = divsInscripcion[i].querySelector('#camiseta');
-
             let generoSeleccionado = '';
+
             for (let j = 0; j < generoRadios.length; j++) {
                 if (generoRadios[j].checked) {
                     generoSeleccionado = generoRadios[j].value;
@@ -158,9 +147,68 @@ export class VistaInscripcion{
                 inputs[9].value, // telefono
                 textarea.value // info
             );
-
             this.inscripciones.push(nuevaInscripcion);
         }
         this.controlador.mostrarConfirmacion(this.inscripciones)
+    }
+
+    /**
+     * Carga las incripciones que han sido rellenas con anterioridad, cuando un usuario vuelve desde la vista
+     * confirmación.
+     */
+    cargarInscripciones(){
+        // Como es una función que se carga al iniciar, primero si la variable ha sido devuelta desde la otra vista confirmación.
+        if(this.inscripcionesVolver.length >=1){
+            // Se recorren y se cargan todas las incripciones con los datos
+            this.inscripcionesVolver.forEach((inscripcion, index) => {
+                if(index == 0){
+                    this.divInscripcion = document.getElementById('divInscripcion-inscripcion-1');
+                }else{
+                    this.divInscripcion = document.createElement('div');
+                    this.divInscripcion.classList.add('card','card-responsive','mt-5');
+                    this.divInscripcion.id = `divInscripcion-inscripcion-${index + 1}`;
+                    let divInscripcionAnterior = document.getElementById(`divInscripcion-inscripcion-${index}`);
+                    this.divInscripcion.innerHTML = divInscripcionAnterior.innerHTML;
+                    divInscripcionAnterior.after(this.divInscripcion);
+                    this.divInscripcion.children[0].querySelector('i').setAttribute('href',`#collapseInscripcion-inscripcion-${index + 1}`);
+                    this.divInscripcion.children[1].id =`collapseInscripcion-inscripcion-${index + 1}`;
+                    this.divInscripcion.querySelector('h1').textContent = `INSCRIPCIÓN ${index+ 1}`;
+                    this.divInscripcion.querySelector(`#collapseInscripcion-inscripcion-${index + 1}`).classList.add('show');
+                    this.fecha1=this.divInscripcion.querySelector('input[type="date"]');
+                    this.fecha1.onchange = this.cargarCategoria.bind(this, this.datos);
+                }
+                //Establece los campos de tipo text
+                this.divInscripcion.querySelector('input[name="nombre"]').value = inscripcion.nombre;
+                this.divInscripcion.querySelector('input[name="apellidos"]').value = inscripcion.apellidos;
+                this.divInscripcion.querySelector('input[name="fecha"]').value = inscripcion.fechaNac;
+                this.divInscripcion.querySelector('input[name="categoria"]').value = inscripcion.categoria;
+                this.divInscripcion.querySelector('input[name="precioDorsal"]').value = inscripcion.precioDorsal +'€';
+                this.divInscripcion.querySelector('input[name="dni"]').value = inscripcion.dni;
+                this.divInscripcion.querySelector('input[name="email"]').value = inscripcion.email;
+                this.divInscripcion.querySelector('input[name="telefono"]').value = inscripcion.telefono;
+                this.divInscripcion.querySelector('textarea[name="infoAdicional"]').value = inscripcion.infoAdicional;
+                this.divInscripcion.querySelectorAll('input[type="radio"][name="genero"]').values();
+                //Establece la opción seleccionada del campo género
+                const generoRadios = this.divInscripcion.querySelectorAll('input[type="radio"][name="genero"]');
+                for (let j = 0; j < generoRadios.length; j++) {
+                    if (generoRadios[j].value === inscripcion.genero) {
+                        generoRadios[j].checked = true;
+                        break;
+                    }
+                }
+                //Establecer la opción seleccionada del campo de selección de camiseta
+                const camisetaSelect = this.divInscripcion.querySelector('#camiseta');
+                for (let k = 0; k < camisetaSelect.options.length; k++) {
+                    if (camisetaSelect.options[k].value === inscripcion.camiseta) {
+                        camisetaSelect.selectedIndex = k;
+                        break;
+                    }
+                }
+                this.inscripcionCounter++;
+                if(index == this.inscripciones.length){
+                    this.inscripcionCounter--;
+                }
+            });
+        }
     }
 }
