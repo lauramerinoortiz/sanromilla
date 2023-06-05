fda204ea2de8391e6f0c4b0423dc031fcebdcf07
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2023-06-05 15:41:06 +0200

    CREATED - validaciones datos-carrera

--- sanromilla_admin/src/js/vistas/carrera/carrera.js
+++ sanromilla_admin/src/js/vistas/carrera/carrera.js
@@ -12,11 +12,12 @@ export class Carrera {
      */
     async iniciar(controlador){
         this.div=document.getElementById('carrera')
+        console.log('div: ',this.div)
 
         this.getInformacion();
 
         this.btnModificar = document.getElementById('modificar');
-        this.btnModificar.onclick = this.modificarInfo();
+        this.btnModificar.onclick = await this.modificarInfo.bind(this);
         this.btnModificar.addEventListener('keypress', function(event) {
             if (event.key === 'Enter'){this.modificarInfo();}
         }.bind(this));
@@ -33,44 +34,22 @@ export class Carrera {
      */
     async modificarInfo(){
 
-        var tabla = document.getElementById("tabla-datos");
-        var inputs = tabla.getElementsByTagName("input");
-        var datos = [];
+        var form = document.getElementById("formInfo");
+        var inputs = form.querySelectorAll('input');
+        var datos = {};
 
-        for (var i = 0; i < inputs.length; i++) {
-            var input = inputs[i];
-            var dorsal = input.value;
-            var id = input.getAttribute("id");
+        inputs.forEach(function(input) {
+            var id = input.id;
+            var value = input.value;
+            datos[id] = value;
+        });
 
-            if (dorsal != ''){
-                datos.push({ dorsal: dorsal, idInscripcion: id });
-            }else{
-                Swal.fire({
-                    title: 'Dorsales vacíos',
-                    text: 'Recuerde rellenar TODOS los dorsales.',
-                    icon: 'warning',
-                    confirmButtonText: 'Vale!'
-                })
-                return 0;
-            }
-        }
-
-        var seteado = await this.controlador.setDorsal(datos);
-
-        if (seteado.data >= 1){
+        var error = this.checkDatos(datos)
+        if(error != ''){
             Swal.fire({
-                title: '¡Dorsales asignados!',
-                text: 'Se han registrado correctamente los dorsales.',
-                icon: 'success',
-                confirmButtonText: 'Vale!'
-            })
-            this.buscarInscripciones();
-        }else{
-            console.log('Error')
-            Swal.fire({
-                title: 'Error en la petición',
-                text: 'Algo no ha ido bien.',
-                icon: 'error',
+                title: 'Algún campo mal',
+                text: error,
+                icon: 'warning',
                 confirmButtonText: 'Vale!'
             })
         }
@@ -82,6 +61,10 @@ export class Carrera {
         (importe <= 0) ? this.btnConfirmar.classList.add('disabled') : this.btnConfirmar.classList.remove('disabled')
     }
 
+    /**
+     * Método para introducir los datos de la información general de la carrera.
+     * @param datos
+     */
     introDatos(datos) {
         var fechaInput = document.getElementById('fecha');
         var cartelInput = document.getElementById('cartel');
@@ -109,5 +92,86 @@ export class Carrera {
         precioCamisetaInput.value = datos.precio_camiseta;
         beneficioCamisetaInput.value = datos.beneficio_camiseta;
 
+    }
+
+    /**
+     * Método para comprobar los datos del formulario.
+     * @param datos
+     * @returns {string} Código de errores.
+     */
+    checkDatos(datos) {
+        var mensaje = '';
+
+        mensaje += this.checkFechaCarrera(datos.fecha)
+        mensaje += this.checkFechasInscripcion(datos)
+        mensaje += this.checkArchivos(datos)
+
+        return mensaje;
+    }
+
+    /**
+     * Método que comprueba que la fecha de la carrera no se posterior al día actual.
+     * @param fecha
+     * @returns {string} Retorna los errores.
+     */
+    checkFechaCarrera(fecha) {
+        var fechaActual = new Date();
+        fechaActual.setHours(0, 0, 0, 0);
+
+        var fechaCarrera = new Date(fecha);
+
+        if (fechaCarrera < fechaActual) { return 'La fecha de la carrera tiene que ser inferior a la actual. '}
+        return '';
+    }
+
+    /**
+     * Método para comprobar las fechas de inscripción, que la inicial no sea superior a la final ni que sea
+     * inferior o igual a la fecha de la carrera.
+     * @param datos
+     */
+    checkFechasInscripcion(datos) {
+        var fechaActual = new Date();
+        fechaActual.setHours(0, 0, 0, 0);
+
+        var fechaCarrera = new Date(datos.fecha);
+        var fechaInicio = new Date(datos.inicio_inscripcion);
+        var fechaFin = new Date(datos.fin_inscripcion);
+
+
+        if (fechaInicio < fechaFin && fechaInicio > fechaCarrera) {
+            return '';
+        } else {
+            return 'Fecha inicio de inscripción que ser inferior a fecha de la carrera y a la fecha de fin de inscripción. ';
+        }
+    }
+
+    /**
+     * Método para comprobar las extensiones de los archivos
+     * @returns {string} Retorna los errores
+     */
+    checkArchivos() {
+        var form = document.getElementById('formInfo');
+        var cartelInput = form.querySelector('#cartel');
+        var reglamentoInput = form.querySelector('#reglamento');
+
+        function isImageFile(file) {
+            var validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+            var fileExtension = file.name.split('.').pop().toLowerCase();
+
+            return validExtensions.includes(fileExtension);
+        }
+
+        for (var i = 0; i < cartelInput.files.length; i++) {
+            if (!isImageFile(cartelInput.files[i])) {
+                return 'El archivo del campo cartel no es una imagen válida';
+            }
+        }
+
+        for (var j = 0; j < reglamentoInput.files.length; j++) {
+            if (!isImageFile(reglamentoInput.files[j])) {
+                return 'El archivo del campo reglamento no es una imagen válida';
+            }
+        }
+        return "";
     }
 }
