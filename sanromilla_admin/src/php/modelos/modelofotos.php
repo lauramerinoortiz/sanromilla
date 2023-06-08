2f589d91b893c68a49d309ea339667e871365ae5
Antonio Moruno <antmoruno4@gmail.com>
2023-06-08 15:07:49 +0200

    UPDATE - ARREGLO RUTA PARA HOSTING

--- sanromilla_admin/src/php/modelos/modelofotos.php
+++ sanromilla_admin/src/php/modelos/modelofotos.php
@@ -26,19 +26,20 @@ class ModeloFotos
         $this->conexion->set_charset("utf8");
     }
 
-    public function subirFotos($archivos, $categoria){
+    public function subirFotos($archivos, $categoria)
+    {
 
         $this->conectar();
 
-        /*if(
+        if (
             isset($categoria) && !empty($categoria)
             && isset($archivos) && !empty($archivos)
-        ){*/
+        ) {
 
             $consulta = $this->conexion->prepare("INSERT INTO imagenes (url, id_categoria)
 														VALUES(?,?);");
             $categoriaRuta = '';
-            switch ($categoria){
+            switch ($categoria) {
                 case 1:
                     $categoriaRuta = 'babyrunner';
                     break;
@@ -68,17 +69,14 @@ class ModeloFotos
 
             foreach ($archivos['tmp_name'] as $indice => $tmp_name) {
                 $nombre_real = $archivos['name'][$indice];
-                $ruta_destino = 'D:\PROGRAMAS\XAMPP\htdocs\sanromilla\sanromilla_admin\src\assets\images\categorias\\'.$categoriaRuta .'\\'. $nombre_real;
-                move_uploaded_file($tmp_name, $ruta_destino);
+                $ruta_destino = "../../src/assets/images/categorias/" . $categoriaRuta . "/" . $nombre_real;
+                $subir = move_uploaded_file($tmp_name, $ruta_destino);
 
                 $consulta->bind_param("si", $nombre_real, $categoria);
                 $consulta->execute();
-
             }
             $consulta->close();
             return true;
-        //}
+        }
     }
-
-
 }
